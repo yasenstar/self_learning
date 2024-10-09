af726f2b35c096218d6ba9f5a201a73422fc54e5
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-10-09 08:46:16 +0800

    add diary tool

---
+++ General_Tools/personal_journaling_tool.md
@@ -0,0 +1,37 @@
+# Tool for Personal Journaling and Diary
+
+Learnt 17 open source tools from https://medevel.com/17-os-journaling-system/#:~:text=In%20this%20article,%20we%20listed%20the#:~:text=In%20this%20article,%20we%20listed%20the
+
+I've digested following four for experiencing, starting from "Mini Diary", will keep eyes on other 3 good candidates. My primary expectation is find one that first work in desktop then mobile.
+
+## MiniDiary
+
+[Mini Diary](https://github.com/samuelmeuli/mini-diary) is a nifty, simple journal app. However, it is not maintained anymore, yet has many contributors and users.
+
+Mini Diary is built using React and uses Electron for building desktop editions.
+
+It comes with an easy-to-use interface that includes search, a note editor with a simple WYSIWYG, a calendar and a word counter.
+
+## Journée
+
+[Journée](https://adueck.github.io/journee/) is a simple journaling desktop application with a clean and clutter-free interface.
+
+The program is released under Apache 2.0 License and is available for Windows, Linux, and macOS.
+
+## Laverna
+
+[Laverna](https://laverna.cc/) is a free, open-source note-taking app with a distraction-free mode. What makes Laverna different is that it supports tasks, code/ syntax highlighting, import/ export and user-friendly keybindings.
+
+Laverna is available for the desktop (macOS, Windows, and Linux) and web as a "self-hosted" edition.
+
+## TiddlyWiki
+
+[TiddlyWiki](https://github.com/TiddlyWiki/TiddlyDesktop/releases) is this post author's personal favorite choice, as it is one compact HTML file with many features and customization options.
+
+TiddlyWiki has a built-in journaling system, full-text search support, tagging options, Wiki-syntax text, and dozens of themes, plugins, languages support.
+
+I highly recommend it for developers, researchers, and software engineers, as it is easy to re-shape according to the user's requirements.
+
+---
+
+Date: 2024/10/09
---
+++ 工程控制论 Engineering Cybernetics.mm
@@ -0,0 +1,103 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="工程控制论&#xa;Engineering Cybernetics" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1728133292666">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="01 引言" POSITION="bottom_or_right" ID="ID_839395430" CREATED="1728133310737" MODIFIED="1728133319266">
+<node TEXT="01.01 常系数线性系统" ID="ID_1994425680" CREATED="1728134087704" MODIFIED="1728134104645"/>
+</node>
+<node TEXT="02 拉氏变换法" POSITION="bottom_or_right" ID="ID_1762926766" CREATED="1728133319446" MODIFIED="1728133341987"/>
+<node TEXT="03 输入、输出和传递函数" POSITION="bottom_or_right" ID="ID_1387489207" CREATED="1728133347833" MODIFIED="1728133358641"/>
+<node TEXT="04 反馈伺服系统" POSITION="bottom_or_right" ID="ID_8358212" CREATED="1728133363030" MODIFIED="1728133371258"/>
+<node TEXT="05 不互相影响的控制" POSITION="bottom_or_right" ID="ID_919102699" CREATED="1728133371432" MODIFIED="1728133385251"/>
+<node TEXT="06 交流伺服系统与振荡控制伺服系统" POSITION="bottom_or_right" ID="ID_1636130243" CREATED="1728133385401" MODIFIED="1728133402068"/>
+<node TEXT="07 采样伺服系统" POSITION="bottom_or_right" ID="ID_701263809" CREATED="1728133402233" MODIFIED="1728133409049"/>
+<node TEXT="08 有时滞的线性系统" POSITION="bottom_or_right" ID="ID_935446289" CREATED="1728133409215" MODIFIED="1728133422229"/>
+<node TEXT="09 平稳随机输入下的线性系统" POSITION="bottom_or_right" ID="ID_1450539290" CREATED="1728133422387" MODIFIED="1728133435013"/>
+<node TEXT="10 继电器伺服系统" POSITION="bottom_or_right" ID="ID_1625853203" CREATED="1728133435187" MODIFIED="1728133441712"/>
+<node TEXT="11 非线性系统" POSITION="bottom_or_right" ID="ID_1552861223" CREATED="1728133447258" MODIFIED="1728133451875"/>
+<node TEXT="12 变系数线性系统" POSITION="bottom_or_right" ID="ID_737166287" CREATED="1728133452033" MODIFIED="1728133462179"/>
+<node TEXT="13 利用摄动理论的控制设计" POSITION="bottom_or_right" ID="ID_939571508" CREATED="1728133462331" MODIFIED="1728133474084"/>
+<node TEXT="14 满足指定积分条件的控制设计" POSITION="bottom_or_right" ID="ID_1589206756" CREATED="1728133474250" MODIFIED="1728133497666"/>
+<node TEXT="15 自动寻求最优运转点的控制系统" POSITION="bottom_or_right" ID="ID_554190355" CREATED="1728133497831" MODIFIED="1728133512937"/>
+<node TEXT="16 噪声过滤的设计原理" POSITION="bottom_or_right" ID="ID_573945693" CREATED="1728133513095" MODIFIED="1728133525171"/>
+<node TEXT="17 自行镇定和适应环境的系统" POSITION="bottom_or_right" ID="ID_400028824" CREATED="1728133525363" MODIFIED="1728133542554"/>
+<node TEXT="18 误差的控制" POSITION="bottom_or_right" ID="ID_1004978206" CREATED="1728133542713" MODIFIED="1728133549429"/>
+</node>
+</map>
