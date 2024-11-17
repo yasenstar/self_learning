d8cf36d73e3ba53a97524988fc9d9f879be32367
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-17 09:31:44 +0800

    Update MS_Learn.mm

--- MS_Learn.mm
+++ MS_Learn.mm
@@ -83,17 +83,196 @@
 </map_styles>
 </hook>
 <node TEXT="MS Azure AI Fundamentals: AI Overview" POSITION="bottom_or_right" ID="ID_321800574" CREATED="1731735908543" MODIFIED="1731735923209">
-<node TEXT="1. Fundamental AI Concepts" POSITION="bottom_or_right" ID="ID_1425870215" CREATED="1731735847735" MODIFIED="1731735935257" LINK="https://learn.microsoft.com/en-us/training/modules/get-started-ai-fundamentals/">
-<node TEXT="1. Introduction to AI" ID="ID_622519490" CREATED="1731735959934" MODIFIED="1731735969586"/>
-<node TEXT="2. Understand Machine Learning" ID="ID_47975042" CREATED="1731735969734" MODIFIED="1731735976138"/>
-<node TEXT="3. Understand Computer Vision" ID="ID_1672000397" CREATED="1731735982743" MODIFIED="1731735990050"/>
-<node TEXT="4. Understand Natural Language Processing (NLP)" ID="ID_330672983" CREATED="1731735990192" MODIFIED="1731736006989"/>
-<node TEXT="5. Understand Document Intelligence and Knowledge Mining" ID="ID_519801208" CREATED="1731736007168" MODIFIED="1731736023089"/>
-<node TEXT="6. Understand Generative AI (GenAI)" ID="ID_1730311795" CREATED="1731736023223" MODIFIED="1731736033841"/>
-<node TEXT="7. Challenges and Risks with AI" ID="ID_12002291" CREATED="1731736034007" MODIFIED="1731736041137"/>
-<node TEXT="8. Understand Responsible AI" ID="ID_254609861" CREATED="1731736041287" MODIFIED="1731736051873"/>
+<node TEXT="1. Fundamental AI Concepts" FOLDED="true" POSITION="bottom_or_right" ID="ID_1425870215" CREATED="1731735847735" MODIFIED="1731735935257" LINK="https://learn.microsoft.com/en-us/training/modules/get-started-ai-fundamentals/">
+<node TEXT="1. Introduction to AI" FOLDED="true" ID="ID_622519490" CREATED="1731735959934" MODIFIED="1731736590803">
+<node TEXT="AI is software that imitates human behaviors and capabilities" ID="ID_109365055" CREATED="1731736590810" MODIFIED="1731736594998"/>
 </node>
-<node TEXT="2. Fundamentals of Machine Learning" POSITION="bottom_or_right" ID="ID_640954668" CREATED="1731735937911" MODIFIED="1731735946395"/>
+<node TEXT="2. Understand Machine Learning" FOLDED="true" ID="ID_47975042" CREATED="1731735969734" MODIFIED="1731735976138">
+<node TEXT="This is often the foundation of an AI system, and is the way we &quot;teach&quot; a computer model to make predictions and draw conclusions from data" ID="ID_909264886" CREATED="1731736578345" MODIFIED="1731736620075">
+<node TEXT="Machine Learning is a branch of AI that combines computer science and mathematics" ID="ID_949999769" CREATED="1731736832186" MODIFIED="1731736850982"/>
+</node>
+<node TEXT="Machine Learning models try to capture the relationship between data." ID="ID_1984775020" CREATED="1731737223598" MODIFIED="1731737244672"/>
+<node TEXT="Azure Machine Learning service" ID="ID_1035001198" CREATED="1731737316037" MODIFIED="1731737329422">
+<node TEXT="Automated machines learning" ID="ID_1733261036" CREATED="1731737329427" MODIFIED="1731737334320"/>
+<node TEXT="Azure Machine Learning designer" ID="ID_1004327959" CREATED="1731737339278" MODIFIED="1731737347936"/>
+<node TEXT="Data metric visualization" ID="ID_1845154910" CREATED="1731737352622" MODIFIED="1731737357719"/>
+<node TEXT="Notebooks" ID="ID_1048552783" CREATED="1731737358797" MODIFIED="1731737361097"/>
+</node>
+</node>
+<node TEXT="3. Understand Computer Vision" FOLDED="true" ID="ID_1672000397" CREATED="1731735982743" MODIFIED="1731735990050">
+<node TEXT="Capabilities within AI to interpret the world visually through cameras, video, and images" ID="ID_426195557" CREATED="1731736620888" MODIFIED="1731736664166">
+<node TEXT="Microsoft Seeing AI web page" ID="ID_827932514" CREATED="1731737430031" MODIFIED="1731737553861" LINK="https://www.seeingai.com/"/>
+</node>
+<node TEXT="Computer Vision models and capabilities" ID="ID_1951541222" CREATED="1731737669382" MODIFIED="1731737676839">
+<node TEXT="Image classification" ID="ID_1640047566" CREATED="1731737676843" MODIFIED="1731737684570">
+<node TEXT="involves training a machine learning model to classify images based on their contents." ID="ID_365239776" CREATED="1731737691910" MODIFIED="1731737710586"/>
+</node>
+<node TEXT="Object detection" ID="ID_1041881962" CREATED="1731737904666" MODIFIED="1731737911011">
+<node TEXT="object detection machine models are trained to classify individual objects within an image, and identify their location with a bounding box" ID="ID_474987536" CREATED="1731737911015" MODIFIED="1731737950492"/>
+</node>
+<node TEXT="Semantic segmentation" ID="ID_661547994" CREATED="1731737967282" MODIFIED="1731737974827">
+<node TEXT="an advanced machine learning technique in which individual pixels in the image are classified according to the object to which they belong" ID="ID_975419322" CREATED="1731737974831" MODIFIED="1731738005740"/>
+</node>
+<node TEXT="Image analysis" ID="ID_1676242404" CREATED="1731738029754" MODIFIED="1731738049586">
+<node TEXT="You can create solutions that combine machine learning models with advanced image analysis techniques to extract information from images, including &quot;tags&quot; that could help catalog the image or even descriptive captions that summarize the scene shown in the image." ID="ID_561727630" CREATED="1731738049590" MODIFIED="1731738053252"/>
+</node>
+<node TEXT="Face detection, analysis, and recognition" ID="ID_748537558" CREATED="1731738055162" MODIFIED="1731738063394">
+<node TEXT="Face detection is a specialized form of object detection that locates human faces in an image" ID="ID_185395838" CREATED="1731738063398" MODIFIED="1731738072702"/>
+</node>
+<node TEXT="Optical character recognition (OCR)" ID="ID_1990137099" CREATED="1731738084186" MODIFIED="1731738098621">
+<node TEXT="Optical character recognition is a technique used to detect and read text in images. You can use OCR to read text in photographs (for example, road signs or store fronts) or to extract information from scanned documents such as letters, invoices, or forms." ID="ID_67819538" CREATED="1731738115626" MODIFIED="1731738118260"/>
+</node>
+</node>
+<node TEXT="Azure Vision Studio" ID="ID_1658673862" CREATED="1731738123691" MODIFIED="1731738130531">
+<node TEXT="Image Analysis" ID="ID_1340810446" CREATED="1731738130536" MODIFIED="1731738135029"/>
+<node TEXT="Face" ID="ID_715680389" CREATED="1731738135298" MODIFIED="1731738136901"/>
+<node TEXT="Optical Character Recognition (OCR)" ID="ID_1228694418" CREATED="1731738137083" MODIFIED="1731738144957"/>
+</node>
+</node>
+<node TEXT="4. Understand Natural Language Processing (NLP)" FOLDED="true" ID="ID_330672983" CREATED="1731735990192" MODIFIED="1731736006989">
+<node TEXT="Capabilities within AI for a computer to interpret written or spoken language, and respond in kind" ID="ID_547734296" CREATED="1731736667130" MODIFIED="1731736697037"/>
+<node TEXT="NPL enables yo to create software that can" ID="ID_1502781336" CREATED="1731738241643" MODIFIED="1731738252162">
+<node TEXT="1. Analyze and interpret text in documents, email messages, and other sources" ID="ID_1429983456" CREATED="1731738252166" MODIFIED="1731738268678"/>
+<node TEXT="2. Interpret spoken language, and synthesize speech responses" ID="ID_573206608" CREATED="1731738268819" MODIFIED="1731738285029"/>
+<node TEXT="3. Automatically translate spoken or written phases between languages" ID="ID_1713461562" CREATED="1731738285243" MODIFIED="1731738301541"/>
+<node TEXT="4. Interpret commands and determine appropriate actions" ID="ID_63119331" CREATED="1731738301731" MODIFIED="1731738314757"/>
+</node>
+<node TEXT="NPL in Microsoft Azure" ID="ID_306950364" CREATED="1731738496052" MODIFIED="1731738502812">
+<node TEXT="Azure AI Language" ID="ID_1319025878" CREATED="1731738502816" MODIFIED="1731738773535">
+<arrowlink DESTINATION="ID_1960941243"/>
+<node TEXT="to build natural language processing solution" ID="ID_1282240574" CREATED="1731738508583" MODIFIED="1731738520447">
+<node TEXT="understanding and analyzing text" ID="ID_1528846540" CREATED="1731738528060" MODIFIED="1731738540173"/>
+<node TEXT="training conversational language models that can understand spoken or text-based commands" ID="ID_1004786422" CREATED="1731738540371" MODIFIED="1731738557462"/>
+<node TEXT="building intelligent applications" ID="ID_1485972930" CREATED="1731738558172" MODIFIED="1731738564205"/>
+</node>
+</node>
+<node TEXT="Azure AI Speech" ID="ID_1221727663" CREATED="1731738575660" MODIFIED="1731738769926">
+<arrowlink DESTINATION="ID_223393124"/>
+<node TEXT="used to build natural language processing solutions" ID="ID_1689694381" CREATED="1731738582835" MODIFIED="1731738593404">
+<node TEXT="speech recognition and synthesis" ID="ID_111914395" CREATED="1731738658948" MODIFIED="1731738669270"/>
+<node TEXT="real-time translations" ID="ID_425456557" CREATED="1731738669420" MODIFIED="1731738673527"/>
+<node TEXT="conversation transcriptions" ID="ID_1978556001" CREATED="1731738673684" MODIFIED="1731738681796"/>
+</node>
+</node>
+<node TEXT="Azure AI Translator" ID="ID_146484708" CREATED="1731738594238" MODIFIED="1731738692381">
+<node TEXT="uses a Neural Machine Translation (NMT) model for translation, which analyzes the semantic context of the text and renders a more accurate and complete translation as a result" ID="ID_1271462812" CREATED="1731738692384" MODIFIED="1731738733053"/>
+</node>
+<node TEXT="Azure Language Studio" ID="ID_1960941243" CREATED="1731738733884" MODIFIED="1731738748542"/>
+<node TEXT="Azure Speech Studio" ID="ID_223393124" CREATED="1731738748692" MODIFIED="1731738760470"/>
+</node>
+</node>
+<node TEXT="5. Understand Document Intelligence and Knowledge Mining" FOLDED="true" ID="ID_519801208" CREATED="1731736007168" MODIFIED="1731736023089">
+<node TEXT="Document Intelligence: Capabilities within AI that deal with managing, processing, and using high volumes of data found in forms and documents." ID="ID_729062594" CREATED="1731736698186" MODIFIED="1731736731037">
+<node TEXT="Azure AI Document Intelligence: can be used and tested in Document Intelligence Studio" ID="ID_1716840650" CREATED="1731738822853" MODIFIED="1731738871694"/>
+</node>
+<node TEXT="Knowledge Mining: Capabilities within AI to extract information from large volumes of often unstructured data to create a searchable knowledge store" ID="ID_803874436" CREATED="1731736731234" MODIFIED="1731736765343">
+<node TEXT="Azure AI Search" ID="ID_1964925097" CREATED="1731738879499" MODIFIED="1731738887846"/>
+</node>
+</node>
+<node TEXT="6. Understand Generative AI (GenAI)" FOLDED="true" ID="ID_1730311795" CREATED="1731736023223" MODIFIED="1731736033841">
+<node TEXT="Capabilities within AI that create original content in a variety of formats including natural language, image, code, and more" ID="ID_1992638591" CREATED="1731736767530" MODIFIED="1731736794686"/>
+<node TEXT="Azure OpenAI service in Azure AI Studio" ID="ID_656250491" CREATED="1731738947924" MODIFIED="1731738960477"/>
+</node>
+<node TEXT="7. Challenges and Risks with AI" FOLDED="true" ID="ID_12002291" CREATED="1731736034007" MODIFIED="1731736041137">
+<node TEXT="Bias can affect result" ID="ID_1757326944" CREATED="1731738990421" MODIFIED="1731739085358">
+<node TEXT="e.g. A loan-approval model discriminates by gender due to bias in the data with which it was trained" ID="ID_1258671997" CREATED="1731739087285" MODIFIED="1731739115718"/>
+</node>
+<node TEXT="Erros may cause harm" ID="ID_1143066237" CREATED="1731739116499" MODIFIED="1731739122013">
+<node TEXT="e.g. an autonomous vehicle experiences a system failure and causes a collision" ID="ID_999199583" CREATED="1731739122016" MODIFIED="1731739142951"/>
+</node>
+<node TEXT="Data could be exposed" ID="ID_435786410" CREATED="1731739143772" MODIFIED="1731739163516">
+<node TEXT="e.g. a medical diagnostic bot is trained using sensitive patient data, which is stored insecurely" ID="ID_577802774" CREATED="1731739163517" MODIFIED="1731739184269"/>
+</node>
+<node TEXT="Solutions may not work for everyone" ID="ID_769600795" CREATED="1731739186339" MODIFIED="1731739192948">
+<node TEXT="e.g. a home automation assistant provides no audio output for visually impaired uses" ID="ID_1993586916" CREATED="1731739192952" MODIFIED="1731739215326"/>
+</node>
+<node TEXT="Users must trust a complex system" ID="ID_1008758135" CREATED="1731739220596" MODIFIED="1731739259077">
+<node TEXT="e.g. an AI-based financial tool makes investment recommendations - what are they based on?" ID="ID_186557954" CREATED="1731739229216" MODIFIED="1731739247885"/>
+</node>
+<node TEXT="Who&apos;s liable for AI-driven decisions?" ID="ID_1281162779" CREATED="1731739248844" MODIFIED="1731739269069">
+<node TEXT="e.g. an innocent person is convicted of a crime based on evidence from facial recognition - whos&apos; responsible?" ID="ID_713567064" CREATED="1731739269072" MODIFIED="1731739290469"/>
+</node>
+</node>
+<node TEXT="8. Understand Responsible AI" ID="ID_254609861" CREATED="1731736041287" MODIFIED="1731736051873">
+<node TEXT="Fairness" ID="ID_489099521" CREATED="1731739354885" MODIFIED="1731739358807">
+<node TEXT="AI systems should treat all people fairly." ID="ID_29802215" CREATED="1731739362125" MODIFIED="1731739371861"/>
+</node>
+<node TEXT="Reliability and Safety" ID="ID_1301900747" CREATED="1731739453484" MODIFIED="1731739459278">
+<node TEXT="AI systems should perform reliably and safety" ID="ID_465559382" CREATED="1731739461396" MODIFIED="1731739494686"/>
+</node>
+<node TEXT="Privacy and Security" ID="ID_1621881776" CREATED="1731739605108" MODIFIED="1731739609581">
+<node TEXT="AI systems should be secure and respect privacy" ID="ID_1681927901" CREATED="1731739609585" MODIFIED="1731739620973"/>
+</node>
+<node TEXT="Inclusiveness" ID="ID_1055161598" CREATED="1731739726532" MODIFIED="1731739730726">
+<node TEXT="AI systems should empower everyone and engage people" ID="ID_660990520" CREATED="1731739730730" MODIFIED="1731739752391"/>
+</node>
+<node TEXT="Transparency" ID="ID_1347635626" CREATED="1731739817847" MODIFIED="1731739823175">
+<node TEXT="AI systems should be understandable" ID="ID_487482893" CREATED="1731739823177" MODIFIED="1731739837970"/>
+</node>
+<node TEXT="Accountability" ID="ID_895951625" CREATED="1731739951474" MODIFIED="1731739954817">
+<node TEXT="People should be accountable for AI systems" ID="ID_244831122" CREATED="1731739954821" MODIFIED="1731739963980"/>
+</node>
+</node>
+</node>
+<node TEXT="2. Fundamentals of Machine Learning" POSITION="bottom_or_right" ID="ID_640954668" CREATED="1731735937911" MODIFIED="1731735946395">
+<node TEXT="1. Introduction" ID="ID_33557493" CREATED="1731740380524" MODIFIED="1731740399734">
+<node TEXT="Machine learning is in many ways the intersection of two disciplines - data science and software engineering." ID="ID_1523222285" CREATED="1731740461349" MODIFIED="1731740463615"/>
+<node TEXT="The goal of machine learning is to use data to create a predictive model that can be incorporated into a software application or service." ID="ID_478568232" CREATED="1731740472805" MODIFIED="1731740475166"/>
+</node>
+<node TEXT="2. What is machine learning?" ID="ID_1041768829" CREATED="1731740399876" MODIFIED="1731740405168">
+<node TEXT="The fundamental idea of machine learning is to use data from past observations to predict unknown outcomes or values." ID="ID_1293382181" CREATED="1731740509839" MODIFIED="1731740512390"/>
+<node TEXT="Machine learning as a function" ID="ID_982758736" CREATED="1731740692726" MODIFIED="1731740695829">
+<node ID="ID_187350090" CREATED="1731740714223" MODIFIED="1731740756964"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Because machine learning is based on mathematics and statistics, it's common to think about machine learning models in mathematical terms. Fundamentally, a machine learning model is a software application that encapsulates a <span style="font-weight: bold; text-decoration: underline;">function</span>&#xa0;&#xa0;to calculate an output value based on one or more input values. The process of defining that function is known as <span style="font-weight: bold; text-decoration: underline;">training</span>. After the function has been defined, you can use it to predict new values in a process called <span style="font-weight: bold; text-decoration: underline;">inferencing</span>.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="3. Types of machine learning" ID="ID_359095429" CREATED="1731740405316" MODIFIED="1731740412271">
+<node TEXT="Supervised machine learning" ID="ID_675563535" CREATED="1731741060251" MODIFIED="1731741067005">
+<node TEXT="Regression" ID="ID_1333528031" CREATED="1731741073691" MODIFIED="1731741104425">
+<arrowlink DESTINATION="ID_24027891"/>
+</node>
+<node TEXT="Classification" ID="ID_1083797199" CREATED="1731741078130" MODIFIED="1731741083714">
+<node TEXT="Binary classification" ID="ID_1690680884" CREATED="1731741083718" MODIFIED="1731741108946">
+<arrowlink DESTINATION="ID_778149451"/>
+</node>
+<node TEXT="Multiclass classification" ID="ID_1693998232" CREATED="1731741090187" MODIFIED="1731741112602">
+<arrowlink DESTINATION="ID_14481467"/>
+</node>
+</node>
+</node>
+<node TEXT="Unsupervised machine learning" ID="ID_473962630" CREATED="1731741067193" MODIFIED="1731741072570">
+<node TEXT="Clustering" ID="ID_132985484" CREATED="1731741072572" MODIFIED="1731741116083">
+<arrowlink DESTINATION="ID_1866082142"/>
+</node>
+</node>
+</node>
+<node TEXT="4. Regression" ID="ID_24027891" CREATED="1731740412437" MODIFIED="1731740416166">
+<node TEXT="Four key elements of the training process for supervised machine learning models" ID="ID_697410162" CREATED="1731742825845" MODIFIED="1731742844605">
+<node TEXT="1. Split the training data (randomly) to create a dataset with which to train the model while holding a subset of the data that you&apos;ll use to validate the trained model" ID="ID_1735736656" CREATED="1731742844609" MODIFIED="1731742889575"/>
+<node TEXT="2. Use an algorithm to fit the training data to a model. In the case of a regression model, use a regression algorithm such as linear regression" ID="ID_1438465052" CREATED="1731742889813" MODIFIED="1731742922592"/>
+<node TEXT="3. Use the validation data you held back to test the model by predicting labels for the features" ID="ID_1374600279" CREATED="1731742922742" MODIFIED="1731742942144"/>
+<node TEXT="4. Compare the know actual labels in the validation dataset to the labels that the model predicted. Then aggregate the differences between the predicted and actual label values to calculate a metric that indicates how accurately the model predicted for the validation data" ID="ID_206463810" CREATED="1731742942358" MODIFIED="1731742997555"/>
+</node>
+</node>
+<node TEXT="5. Binary classification" ID="ID_778149451" CREATED="1731740416333" MODIFIED="1731740421750"/>
+<node TEXT="6. Multiclass classification" ID="ID_14481467" CREATED="1731740421924" MODIFIED="1731740430271"/>
+<node TEXT="7. Clustering" ID="ID_1866082142" CREATED="1731740430421" MODIFIED="1731740434839"/>
+<node TEXT="8. Deep learning" ID="ID_1000048957" CREATED="1731740434997" MODIFIED="1731740439390"/>
+<node TEXT="9. Azure Machine Learning" ID="ID_954342887" CREATED="1731740439542" MODIFIED="1731740444175"/>
+</node>
 <node TEXT="3. Fundamentals of Azure AI Services" POSITION="bottom_or_right" ID="ID_1120621644" CREATED="1731735946551" MODIFIED="1731735954057"/>
 </node>
 </node>
