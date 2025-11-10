10a9575df087d43e5228c0e16b07e6c569a74383
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-11-10 16:43:14 -0500

    update video cover image

--- my-node-app/index.js
+++ my-node-app/index.js
@@ -3,8 +3,10 @@ import { encode } from '@toon-format/toon';
 const data = {
   users: [
     { id: 1, name: 'Alice', role: 'admin' },
-    { id: 2, name: 'Bob', role: 'user' }
+    { id: 2, name: 'Bob', role: 'user' },
+    { id: 3, name: 'Jack', role: 'user' }
   ]
 }
 
+console.log(data);
 console.log(encode(data))
